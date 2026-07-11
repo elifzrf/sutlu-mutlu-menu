a288f8133a0c12d52a0a61a1ccfe22127a62ecfa
elif <130744252+elifzrf@users.noreply.github.com>
2026-07-12 00:26:58 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
+function changeLanguage(lang){
+    alert("Dil seçildi: " + lang);
+}
 
+function searchProduct(){
+    let input=document.getElementById("searchInput").value.toLowerCase();
+    let cards=document.querySelectorAll(".card");
+
+    cards.forEach(card=>{
+        let title=card.querySelector("h2").innerText.toLowerCase();
+
+        if(title.includes(input)){
+            card.style.display="block";
+        }else{
+            card.style.display="none";
+        }
+    });
+}
